c42f4f1bed07e55a661d4858dfdf7a2481e3bac9
stehrenberg <sehrenbe@hm.edu>
2017-06-02 22:56:27 +0200

    Clean up unneccessary files and set up build process

---
+++ src/build.py
@@ -0,0 +1,15 @@
+from pybuilder.core import init, use_plugin
+
+use_plugin("python.core")
+use_plugin("python.install_dependencies")
+
+default_task = ['install_dependencies']
+
+@init
+def initialize(project):
+  project.set_property('dir_source_main_python', '/src')
+  project.set_property('dir_install_logs', 'logs')
+  init_dependencies(project)
+
+def init_dependencies(project):
+    project.build_depends_on('unicornhat')

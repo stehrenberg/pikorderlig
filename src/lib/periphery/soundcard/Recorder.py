7de43bf8e9c4b4dd5a21b5467632222108e1c635
stehrenberg <sehrenbe@hm.edu>
2017-06-03 00:33:03 +0200

    First version of actual recorder

--- src/lib/periphery/soundcard/Recorder.py
+++ src/lib/periphery/soundcard/Recorder.py
@@ -1,14 +1,41 @@
 import soundfile as sf
 import sounddevice as sd
+import queue
 
 class Recorder:
 
     def __init__(self, filepath_as_string):
-        self.filepath = filepath_as_string
+        self._filepath = filepath_as_string
+        self._is_recording = False
+        self._samplerate = 44100
+        self._channels = 2
+        self._subtype = "PCM_16"
+        self._queue = queue.Queue()
 
     def start(self):
         print("*** Recording started!")
         print(sd.query_devices())
+        self._is_recording = True
+
+        with sf.SoundFile(self._filepath,
+                mode='x',
+                samplerate=self._samplerate,
+                channels=self._channels,
+                subtype=self._subtype) as file:
+            with sd.InputStream(samplerate=self._samplerate,
+                                device=0,
+                                channels=self._channels,
+                                callback=self._callback):
+                while self._is_recording:
+                    file.write(self._queue.get())
+
 
     def stop(self):
         print("*** Recording stopped")
+        self._is_recording = False
+
+    def _callback(self, indata, frames, time, status):
+        self._queue.put(indata.copy())
+
+
+
--- src/pikorderlig.py
+++ src/pikorderlig.py
@@ -6,11 +6,14 @@ print("*** Hi there! Starting up...")
 
 date_format = '%Y-%m-%d_%H-%M-%S'
 date_as_string = time.strftime(date_format)
-record_file = 'recording_' + date_as_string
+base_path = '/home/pi/recordings/'
+file_ending = '.wav'
+record_file = 'recording_' + date_as_string + file_ending
+recorder = Recorder(record_file)
+
 
 print("time.strftime() is: " + date_as_string)
 print("file name is: " + record_file)
-recorder = Recorder(record_file)
 
 recorder.start()
 time.sleep(5)

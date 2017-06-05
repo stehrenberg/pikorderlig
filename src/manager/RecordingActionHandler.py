600cc20480ce1222d4eef59e055c791b4eaa9146
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-06-05 23:36:40 +0200

    Python coding style

--- src/manager/RecordingActionHandler.py
+++ src/manager/RecordingActionHandler.py
@@ -4,9 +4,9 @@ class RecordingActionHandler:
     def __init__(self, recorder):
         self._recorder = recorder
         self._action_mappings = {
-            'recording:start' : self._recorder.start_recording,
-            'recording:stop' : self._recorder.stop_recording,
-            'recording:get_status' : self._recorder.get_status
+            'recording:start': self._recorder.start_recording,
+            'recording:stop': self._recorder.stop_recording,
+            'recording:get_status': self._recorder.get_status
         }
 
     def get_action_mappings(self):

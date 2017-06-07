7fe7fec3a742825462272dd96fced0e0fba2e2d1
Dominik Ehrenberg <Dominik@Jungowski.de>
2017-06-07 15:52:59 +0200

    create new recording slice every 2 hours

--- src/lib/periphery/soundcard/LineInGrabber.py
+++ src/lib/periphery/soundcard/LineInGrabber.py
@@ -6,6 +6,15 @@ from threading import Thread
 
 
 class LineInGrabber(Thread):
+    _is_recording = False
+    _filepath = ''
+    _current_slice = 0
+    _current_slice_start = 0.0
+    _slice_length = 7200.0  # seconds
+    _samplerate = 44100
+    _channels = 2
+    _subtype = "PCM_16"
+
     """
     Represents a recording instance that records the current line-in signal as .WAV file.
     """
@@ -13,28 +22,22 @@ class LineInGrabber(Thread):
     def __init__(self, file_information):
         Thread.__init__(self)
         self._file_information = file_information
-        self._filepath = ''
-        self._is_recording = False
-        self._samplerate = 44100
-        self._channels = 2
-        self._subtype = "PCM_16"
-        self._queue = queue.Queue()
+        self._recording_queue = queue.Queue()
         self._recording_start = 0
-        self._current_slice = 0
 
     def run(self):
         self._is_recording = True
         self.record()
 
-    def _get_current_slice(self):
+    def _get_current_slice_as_string(self):
         return str(self._current_slice)
 
     def _get_current_file_name(self):
         return self._file_information['file_path'] +\
-               '-part-' + self._get_current_slice() +\
+               '-part-' + self._get_current_slice_as_string() +\
                self._file_information['file_ending']
 
-    def _create_sound_file(self):
+    def _create_next_sound_file(self):
         self._filepath = self._get_current_file_name()
         self._soundfile = sf.SoundFile(self._filepath,
                                        mode='x',
@@ -42,19 +45,26 @@ class LineInGrabber(Thread):
                                        channels=self._channels,
                                        subtype=self._subtype)
 
+        self._reset_slice_length()
+
     def record(self):
         self._recording_start = time.time()
-        self._create_sound_file()
-        print('Recording ', self._filepath)
+        self._create_next_sound_file()
+        print('*** Recording ', self._filepath)
 
         # with self._soundfile as file:
         with sd.InputStream(samplerate=self._samplerate,
                             device=0,
                             channels=self._channels,
-                            callback=self._callback):
+                            callback=self._input_stream_callback):
             while self._is_recording:
-                self._soundfile.write(self._queue.get())
+                self._soundfile.write(self._recording_queue.get())
         return
+
+    def _rotate_slice(self):
+        self._current_slice += 1
+        self._create_next_sound_file()
+        print('*** Recording new slice ', self._filepath)
 
     def stop(self):
         self._is_recording = False
@@ -62,13 +72,16 @@ class LineInGrabber(Thread):
         self._soundfile.close()
         print("*** Stopped recording ", self._filepath)
 
-    def is_recording(self):
-        return self._is_recording
-
-    def _callback(self, indata, frames, time, status):
+    def _input_stream_callback(self, indata, frames, recording_time, status):
         if status:
-            print("Error while recording: ", status)
-        self._queue.put(indata.copy())
+            print("*** Error while recording: ", status)
+
+        self._store_slice_start(recording_time)
+
+        if self._is_slice_length_reached(recording_time):
+            self._rotate_slice()
+
+        self._recording_queue.put(indata.copy())
 
     def get_status(self):
         return {
@@ -76,6 +89,19 @@ class LineInGrabber(Thread):
             "file": self._filepath,
             "recording_start": self._recording_start
         }
+
+    def is_recording(self):
+        return self._is_recording
+
+    def _is_slice_length_reached(self, recording_time):
+        return recording_time.currentTime - self._current_slice_start >= self._slice_length
+
+    def _reset_slice_length(self):
+        self._current_slice_start = 0.0  # will be set first time in the input callback
+
+    def _store_slice_start(self, recording_time):
+        if self._current_slice_start == 0.0:
+            self._current_slice_start = recording_time.currentTime
 
     def sigIntHandler(self, signum, stack):
         print("*** Keyboard interrupt, stopping recording")
